404423f082ece6e72b7ce8566519d40d52ee74b4
lanaafana <lanaafana@gmail.com>
2023-02-15 10:06:22 +0300

    Initial actions: - creating DB - adding dependencies (Postgresql, Jooq, Kafka) - creating project structure

---
+++ src/main/kotlin/com/lanaafana/automobile/AutomobileApplication.kt
@@ -0,0 +1,11 @@
+package com.lanaafana.automobile
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutomobileApplication
+
+fun main(args: Array<String>) {
+    runApplication<AutomobileApplication>(*args)
+}
---
+++ src/test/kotlin/com/lanaafana/automobile/AutomobileApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lanaafana.automobile
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutomobileApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
