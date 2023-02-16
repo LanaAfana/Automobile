11ff124547ac9febdc8c0469590fc7bfcc73978c
Дмитрий Князьков <medavedik@yandex.ru>
2023-02-16 10:45:16 +0300

    Обратная связь по ТЗ.

--- src/main/kotlin/com/lanaafana/automobile/businessLayer/Auto.kt
+++ src/main/kotlin/com/lanaafana/automobile/businessLayer/Auto.kt
@@ -1,21 +1,25 @@
 package com.lanaafana.automobile.businessLayer
 
-import com.google.gson.Gson
-
-fun <T> toJson(obj: T): String {
-    return Gson().toJson(obj);
-}
+//import com.google.gson.Gson
+//
+//fun <T> toJson(obj: T): String {
+//    return Gson().toJson(obj); - плохая пркактика создавать каждый раз объект, только для того чтобы вызвать "toJson(obj):
+//}
 data class Auto(
-    private var id: Long,
-    private var brand: String,
-    private var model: String,
-    private var color: String,
-    private var engineNumber: String
+//    private var id: Long,  - зачем тут private(мы не увидим этих свойств на фронте)  и мутабельные свойства(var),
+//    private var brand: String,
+//    private var model: String,
+//    private var color: String,
+//    private var engineNumber: String
 
-
+    val id: Long,
+    val brand: String,
+    val model: String,
+    val color: String,
+    val engineNumber: String
 )
 
 data class Test(
-        private var id: Long,
-        private var engineNumber: String
+    val id: Long,
+    val engineNumber: String
 )
--- src/main/kotlin/com/lanaafana/automobile/persistence/AutoRepository.kt
+++ src/main/kotlin/com/lanaafana/automobile/persistence/AutoRepository.kt
@@ -2,37 +2,42 @@ package com.lanaafana.automobile.persistence
 
 import com.lanaafana.automobile.businessLayer.Auto
 import com.lanaafana.automobile.businessLayer.Test
-import com.lanaafana.automobile.domain.Tables
+import com.lanaafana.automobile.jooq.Tables
 import org.jooq.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class AutoRepository (
-    private val dslContext: DSLContext)
-{
+class AutoRepository(
+    private val dslContext: DSLContext,
+) {
     private val auto = Tables.AUTO
     private val brand = Tables.BRAND
     private val model = Tables.MODEL
     private val color = Tables.COLOR
 
-    fun selectAllAuto(): List<Auto>  { //Result<Record3<Int!, String!, String!>>
+    fun selectAllAuto(): List<Auto> { //Result<Record3<Int!, String!, String!>>
         return dslContext
-                .select(auto.ID, brand.NAME, model.NAME, color.COLOR_, auto.ENGINE_NUMBER)
-                .from(auto)
-                .join(brand).on(brand.ID.eq(auto.BRAND_ID))
-                .join(model).on(model.ID.eq(auto.MODEL_ID))
-                .join(brand).on(color.ID.eq(auto.COLOR_ID))
-                .fetch() //Records.mapping(Auto::new))
-                .map { it.into(Auto::class.java) }
-
+            .select(
+                auto.ID,
+                brand.NAME.`as`("brand"), // если явно не указать имена свойствам,
+                model.NAME.`as`("model"), // то ничего не смапиться автоматом, так как имена в дата класе не совпадают с именами свойст в запросе
+                color.COLOR_.`as`("color"), // + еще "name" повторяется 2-раза
+                auto.ENGINE_NUMBER
+            )
+            .from(auto)
+            .join(brand).on(brand.ID.eq(auto.BRAND_ID))
+            .join(model).on(model.ID.eq(auto.MODEL_ID))
+            .join(color).on(color.ID.eq(auto.COLOR_ID))
+            // .join(brand).on(color.ID.eq(auto.COLOR_ID)) // "джойнится" второй раз таблица брендов
+            // .fetch() //Records.mapping(Auto::new))
+            // .map { it.into(Auto::class.java) }
+            .fetchInto(Auto::class.java) // лучше использовать маппер который предастовляет jooq
     }
 
-    fun selectAllTest(): Result<Record2<Int, String>> {
+    fun selectAllTest(): List<Test> { // Result<Record2<Int, String>>  - плохая практика передавать результат в сыром виде, с учетом того, что для результата есть уже обертка
         return dslContext
-                .select(auto.ID, auto.ENGINE_NUMBER)
-                .from(auto)
-                .fetch()
-
-
+            .select(auto.ID, auto.ENGINE_NUMBER)
+            .from(auto)
+            .fetchInto(Test::class.java) // лучше использовать маппер который предастовляет jooq
     }
 }
